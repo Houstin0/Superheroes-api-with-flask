f433b9d2e26b546f3b970cceaed97e6d2fc4b8ec
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-10-02 14:37:50 +0300

    working backend

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,jsonify,request
 from flask_migrate import Migrate
+from flask_restful import Resource,Api
 
-from models import db, Hero
+from models import db, Hero,Power,HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -13,10 +14,143 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return ''
+api = Api(app)
+
+class Index(Resource):
+    def get(self):
+        try:
+            response_dict = {
+                "index": "Welcome to the Hero RESTful API",
+            }
+            return make_response(jsonify(response_dict), 200)
+            
+        except Exception as e:
+            response_dict = {"error": "An error occurred."}
+            return make_response(jsonify(response_dict), 500)
+
+api.add_resource(Index, '/')
+
+class Heroes(Resource):
+    def get(self):
+        try:
+            heroes=Hero.query.all()
+            heroes_dicts=[hero.to_dict() for hero in heroes]
+            return make_response(jsonify(heroes_dicts),200)
+        
+        except Exception as e:
+            response_dict = {"error": f"An error occurred while fetching heroes.{str(e)}"}
+            return make_response(jsonify(response_dict), 500)
+        
+api.add_resource(Heroes,'/heroes')   
+
+class HeroByID(Resource):
+    def get (self,id):
+        try:
+            hero=Hero.query.filter_by(id=id).first()
+            if hero:
+                hero_dict=hero.to_dict()
+                return make_response(jsonify(hero_dict),200)
+            else:
+                response_dict = {"error": "Hero not found"}
+                return make_response(jsonify(response_dict), 404)
+
+        except Exception as e:
+            response_dict = {"error": f"An error occurred while fetching hero by ID.{str(e)}"}
+            return make_response(jsonify(response_dict), 500)
+        
+api.add_resource(HeroByID,'/heroes/<int:id>')   
+
+class Powers(Resource):
+    def get(self):
+        try:
+            powers=Power.query.all()
+            powers_dicts=[power.to_dict() for power in powers]
+            return make_response(jsonify(powers_dicts),200)
+        
+        except Exception as e:
+            response_dict = {"error": f"An error occurred while fetching powers.{str(e)}"}
+            return make_response(jsonify(response_dict), 500)
+        
+api.add_resource(Powers,'/powers')  
+
+class PowerByID(Resource):
+    def get (self,id):
+        try:
+            power=Power.query.filter_by(id=id).first()
+            if power:
+                power_dict=power.to_dict()
+                return make_response(jsonify(power_dict),200)
+            else:
+                response_dict = {"error": "Power not found"}
+                return make_response(jsonify(response_dict), 404)
+
+        except Exception as e:
+            response_dict = {"error": f"An error occurred while fetching power by ID.{str(e)}"}
+            return make_response(jsonify(response_dict), 500)
+        
+    def patch(self,id):
+        data = request.get_json()
+        try:
+            power= Power.query.filter_by(id=id).first()
+            if power:
+                for attr in data:
+                    setattr(power,attr,data.get(attr))
+                db.session.add(power)
+                db.session.commit()
+
+                response_dict=power.to_dict()
+                return make_response(jsonify(response_dict),200)   
+            else:
+                response_dict = {"error": "Power not found"}
+                return make_response(jsonify(response_dict), 404)
+
+            
+        except Exception as e:
+            response_dict = {"error": f"{str(e)}"}
+            return make_response(jsonify(response_dict), 500)    
+        
+api.add_resource(PowerByID,'/powers/<int:id>')
+
+class HeroPowers(Resource):
+    def get(self):
+        try:
+            hero_powers=HeroPower.query.all()
+            hero_powers_dicts=[hero_powers.to_dict() for hero_powers in hero_powers]
+            return make_response(jsonify(hero_powers_dicts),200)
+            
+        
+        except Exception as e:
+            response_dict = {"error": "An error occurred while fetching hero_powers."}
+            return make_response(jsonify(response_dict), 500)
+
+     
+    def post(self):
+        data = request.get_json()
+        try:
+            new_hero_powers=HeroPower(
+                strength=data.get('strength'),
+                power_id= data.get("power_id"),
+                hero_id= data.get("hero_id"),
+            ) 
+            db.session.add(new_hero_powers)
+            db.session.commit()
+        
+            response = make_response(
+                new_hero_powers.to_dict(),
+                201 
+            )
+            return response
+        
+        except Exception as e:
+            response_dict = {"error" : f"{str(e)}"}
+            return make_response(jsonify(response_dict), 403)
+
+    
+api.add_resource(HeroPowers, '/hero_powers')
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555,debug=True)
--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
 class Power(db.Model,SerializerMixin):
     __tablename__ = 'powers'
 
-    serialize_rules =()
+    serialize_rules =('-heroes','-hero_powers')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String,nullable=False)
@@ -14,17 +15,24 @@ class Power(db.Model,SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    hero_powers = db.relationship('HeroPower', back_populates='powers')
-    heroes=db.relationship('Hero',secondary='hero_powers',back_populates='powers')
+    hero_powers = db.relationship('HeroPower', back_populates='powers',overlaps="powers")
+    heroes=db.relationship('Hero',secondary='hero_powers',back_populates='powers',overlaps="hero_powers")
 
     def __repr__(self):
-        return f'Power {self.id}, name: {self.name} description: {self.description})'
+        return f'Power {self.id} | name: {self.name} | description: {self.description})'
+    
+    @validates('description')
+    def checks_description(self, key, description):
+        if len(description) < 20:
+            raise ValueError("Description must be longer than 20 chars")
+        else:
+            return description
 
 
 class Hero(db.Model,SerializerMixin):
     __tablename__ = 'heroes'
 
-    serialize_rules = ()
+    serialize_rules = ('-hero_powers','powers')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True,nullable=False)
@@ -32,18 +40,18 @@ class Hero(db.Model,SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    hero_powers = db.relationship('HeroPower', back_populates='heroes')
-    powers=db.relationship('Power',secondary='hero_powers',back_populates='heroes')
+    hero_powers = db.relationship('HeroPower', back_populates='heroes',overlaps="heroes")
+    powers=db.relationship('Power',secondary='hero_powers',back_populates='heroes',overlaps="hero_powers")
 
     def __repr__(self):
-        return f'(Hero {self.id}, name: {self.name} super_name: {self.super_name})'
+        return f'(Hero {self.id} | name: {self.name} | super_name: {self.super_name})'
 
 
 
 class HeroPower(db.Model,SerializerMixin):
     __tablename__ = 'hero_powers'
 
-    serialize_rules =()
+    serialize_rules =('-heroes','-powers')
 
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String)
@@ -52,12 +60,19 @@ class HeroPower(db.Model,SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    heroes = db.relationship('Hero', back_populates='hero_powers')
-    powers = db.relationship('Power', back_populates='hero_powers')
+    heroes = db.relationship('Hero', back_populates='hero_powers',overlaps="heroes,powers")
+    powers = db.relationship('Power', back_populates='hero_powers',overlaps="heroes,powers")
 
 
     def __repr__(self):
-        return f'(HeroPower {self.id}, heroID: {self.hero_id} strength: {self.strength}) powerID: {self.power_id}'
+        return f'(HeroPower {self.id} | heroID: {self.hero_id} | strength: {self.strength}) powerID: {self.power_id}'
+    
+    @validates('strength')
+    def checks_strength(self, key, strength):
+        if strength not in ['Strong', 'Weak', 'Average']:
+            raise ValueError("Strength must be a value either 'Strong', 'Weak' or 'Average'")
+        else:
+            return strength
 
 
 
